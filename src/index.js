a9739b9e4b6a97e96ac93c2c55a0a6b9ce408267
FUNGCHAN\qwert <tchan199616@gmail.com>
2024-05-22 04:29:54 +0800

    ➕ | Handle basic DOM tree rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { renderTree } from "./render"
 
 export function create(menuData, target, config) {
+  const container = document.querySelector(`[data-menuy="${target}"]`);
   const menuTree = renderTree(menuData)
+  container.appendChild(menuTree)
   return menuTree
 }
--- src/render.js
+++ src/render.js
@@ -1,3 +1,22 @@
-export function renderTree(menuData) {
-  console.log('rendered', menuData)
+export function renderTree(menuData, layer = 1) {
+  const ul = document.createElement('ul')
+
+  for (const item of menuData) {
+    const li = document.createElement('li')
+    const liItem = document.createElement('button')
+    liItem.textContent = item.name
+    liItem.addEventListener('click', () => {
+      window.location.href = '#';
+    })
+    li.appendChild(liItem)
+
+    if (item.children) {
+      const childUl = renderTree(item.children, layer + 1)
+      li.appendChild(childUl)
+    }
+
+    ul.appendChild(li)
+  }
+
+  return ul
 }
